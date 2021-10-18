11e68ee37f4a4e5796e225b685c0b557abd653cf
Mark Needham <m.h.needham@gmail.com>
2021-10-18 16:10:35 +0100

    Update test_insights.py

--- sdk/tests/test_insights.py
+++ sdk/tests/test_insights.py
@@ -1,13 +1,13 @@
 import unittest
 from finbourne_insights import api as ia
 from fbnsdkutilities import ApiClientFactory
-
+import finbourne_insights
 
 class InsightsTests(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        api_client = ApiClientFactory(ia, api_secrets_filename="secrets.json")
+        api_client = ApiClientFactory(finbourne_insights, api_secrets_filename="secrets.json")
         cls.requests_api = api_client.build(ia.RequestsApi)
 
     def test_requests(self):
